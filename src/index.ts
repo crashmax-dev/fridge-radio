f485bd845be69444600a7b67f4fc9b51c9e130b8
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-03-11 02:40:10 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export default class FooBar {
+  #foo: string
+  #bar: number
+
+  constructor(foo: string, bar: number) {
+    this.#foo = foo
+    this.#bar = bar
+  }
+
+  get foobar(): string {
+    return `${this.#foo} ${this.#bar}`
+  }
+}
